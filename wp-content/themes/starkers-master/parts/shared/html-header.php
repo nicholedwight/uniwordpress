07eacaab6455caba34be5c956d0cd17988721897
Nichole Dwight <nicholedwight@gmail.com>
2014-03-02 20:35:04 +0000

    Adding Sail font

--- wp-content/themes/starkers-master/parts/shared/html-header.php
+++ wp-content/themes/starkers-master/parts/shared/html-header.php
@@ -12,6 +12,7 @@
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Sail">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
