4443aad6d46901ed488adec51792bbfcd08f278c
Nichole Dwight <nicholedwight@gmail.com>
2014-03-02 12:13:53 +0000

    Added pagecontainer and styling

--- wp-content/themes/starkers-master/page.php
+++ wp-content/themes/starkers-master/page.php
@@ -17,8 +17,10 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<!--<h2><?php the_title(); ?></h2>-->
-<?php the_content(); ?>
+<div class="pagecontainer">
+	<!--<h2><?php the_title(); ?></h2>-->
+	<?php the_content(); ?>
+</div>
 <?php comments_template( '', true ); ?>
 <?php endwhile; ?>
 
