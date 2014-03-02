4df168b36483a951c445ad3a6417c36ca4181474
Nichole Dwight <nicholedwight@gmail.com>
2014-03-02 13:45:23 +0000

    Tweaked postcontainer and content in Footer

--- wp-content/themes/starkers-master/single.php
+++ wp-content/themes/starkers-master/single.php
@@ -13,7 +13,7 @@
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-<article>
+<article class="postcontainer">
 
 	<h2><?php the_title(); ?></h2>
 	<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
